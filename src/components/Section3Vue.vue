af9cc0745f8e5ca61eb092be668dd6e64ffbf6b4
wa26532977 <peterwang_123@yahoo.com>
2022-05-15 23:27:22 -0400

    05/15/2022 home2

--- src/components/Section3Vue.vue
+++ src/components/Section3Vue.vue
@@ -1,6 +1,15 @@
 <template>
 <div>
-  {{welcome}}
+  <h1>{{welcome}}</h1>
+  <h3>My course goals</h3>
+  <input type="text" v-model="goal" @keyup.enter="addGoal">
+  <button @click="addGoal">Add Goal</button>
+  <p v-if="goals.length === 0">No goals have been added yet-please start adding some</p>
+  <ul v-else>
+    <li v-for="(g, index) in goals" :key="index">
+      {{g}}
+    </li>
+  </ul>
 </div>
 </template>
 
@@ -9,7 +18,15 @@ export default {
   name: 'Section3Vue',
   data () {
     return {
-      welcome: 'welcome to section 3'
+      welcome: 'welcome to section 3',
+      goals: [],
+      goal: ''
+    }
+  },
+  methods: {
+    addGoal () {
+      this.goals.push(this.goal)
+      this.goal = ''
     }
   }
 }
